502cafa9b72e6a7915b52ec4196a3c8e87c0d20f
DYNOH812 <munyuadennis@gmail.com>
2023-10-11 21:50:50 +0300

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,3 +15,7 @@ const app = express();
 app.listen(3000,() => {
     console.log('listening on port 3000');
 });
+
+//path: api/index.js
+
+app.use('/api/user', userRoutes);
